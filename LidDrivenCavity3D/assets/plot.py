6f7ac8f05f80dd1bf3a8e006bad3b76c4ed8df26
Gregor Olenik <go@hpsim.de>
2023-12-22 17:11:00 +0100

    install seaborn plot

--- LidDrivenCavity3D/assets/plot.py
+++ LidDrivenCavity3D/assets/plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sb
 
 from pathlib import Path
 
@@ -10,6 +11,14 @@ json_file = post_pro_dir / "results.json"
 df = pd.read_json(json_file)
 
 fig, axs = plt.subplots(figsize=(12, 4))
-#df.plot(ax=axs)
+
+x = "nCells"
+y = "TimeStep"
+color = "nProcs"
+style = "solver_p"
+plot_type = "line"
+
+
+sb.relplot(x="nCells", y="TimeStep", hue=color, hue_order=hue_order, size=units, style=style, data=df, col="Host", kind="line", markers=True)
 
 fig.savefig(post_pro_dir / "TimeStep.png")
