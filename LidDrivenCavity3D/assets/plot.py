dde7d213dc9b5c6fa75c7112ad420e006a04e14f
Gregor Olenik <gregor.olenik@web.de>
2024-01-10 15:59:25 +0100

    add comparisson plots

--- LidDrivenCavity3D/assets/plot.py
+++ LidDrivenCavity3D/assets/plot.py
@@ -34,6 +34,8 @@ def plotter(
 
 def col_divide(df_orig, df_comparisson):
     ret = deepcopy(df_orig)
+    df_orig = df_orig.set_index("jobid")
+    df_comparisson = df_comparisson.set_index("jobid")
     for c in df_orig.columns:
         if c == "nCells" or c == "nProcs":
             continue
