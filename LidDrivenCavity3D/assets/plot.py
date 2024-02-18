550c94de326076706c984740851ab075ef0d62aa
Gregor Olenik <gregor.olenik@web.de>
2024-02-18 07:42:31 +0100

    update postpro workflow

--- LidDrivenCavity3D/assets/plot.py
+++ LidDrivenCavity3D/assets/plot.py
@@ -3,10 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
 import exasim_plot_helpers as eph
-from pathlib import Path
-from copy import deepcopy
+import logging
 
 from pathlib import Path
+from copy import deepcopy
+from pathlib import Path
+
 
 def plot_script():
     return """import pandas as pd
@@ -15,15 +17,37 @@ df_json = '{}'
 df = pd.DataFrame.read_json(df_json)
     """
 
+
+def palette():
+    return sb.color_palette("tab10")
+
+
 def plotter(
-    x, y, color, style, df, df_filter, post_pro_dir, postfix="", size=None, col=None, log=None, plot_type="line"
+    x,
+    y,
+    color,
+    style,
+    df,
+    df_filter,
+    post_pro_dir,
+    postfix="",
+    size=None,
+    col=None,
+    log=None,
+    plot_type="line",
 ):
     df = df_filter(df)
+    if df.empty:
+        logging.warning("Dataframe empty after filter")
     name = f"{df_filter.name}_{y}_over_{x}_c={color}_s={style}_cols={col}{postfix}"
-    script_name = name+".py"
+    script_name = name + ".py"
 
-    with open (post_pro_dir / script_name, "w") as script:
+    script_dir = post_pro_dir / "scripts" 
+    script_dir.mkdir(parents=True, exist_ok=True)
+
+    with open(script_dir / script_name, "w") as script:
         script.write(plot_script().format(df.to_json()))
+
 
     relplot = sb.relplot(
         x=x,
@@ -35,6 +59,7 @@ def plotter(
         col=col,
         kind=plot_type,
         markers=True,
+        palette=palette(),
     )
     if log == "both":
         plt.xscale("log")
@@ -42,8 +67,7 @@ def plotter(
     if log == "x":
         plt.xscale("log")
     fig_name = name + ".png"
-    plt.savefig(post_pro_dir / fig_name )
-
+    plt.savefig(post_pro_dir / fig_name)
 
 
 def col_divide(df_orig, df_comparisson):
@@ -54,14 +78,15 @@ def col_divide(df_orig, df_comparisson):
         if c == "nCells" or c == "nProcs":
             continue
         try:
-            print(f"c: {c} ret[c] {df_comparisson[c] / df_orig[c]}  comp:  {df_comparisson[c]} orig: {df_orig[c]}")  
-            ret[c] = df_comparisson[c] / df_orig[c]  
+            print(
+                f"c: {c} ret[c] {df_comparisson[c] / df_orig[c]}  comp:  {df_comparisson[c]} orig: {df_orig[c]}"
+            )
+            ret[c] = df_comparisson[c] / df_orig[c]
         except Exception as e:
             print(e, c)
             pass
     print(f"df[TimeStep] {ret['TimeStep']}")
     return ret
-
 
 
 class Df_filter:
@@ -70,21 +95,97 @@ class Df_filter:
         self.func = func
 
     def __call__(self, df):
-        return self.func(df)
+        return self.func(deepcopy(df))
 
 
-def compute_speedup(df, bases, extra_filter=lambda df: df):
-    from copy import deepcopy
+def compute_speedup(df, bases, extra_filter=lambda df: df, node_based=False):
+    # check if bases vals are in df
+    bases_clean = []
+    for record in bases:
+        base = record["base"]
+        keep = all([query.val in df[query.idx].values for query in base])
+        if keep:
+            bases_clean.append(record)
+    bases=bases_clean
 
-    indices = [q.idx for q in bases[0]["base"]]
-    indices.append("Host")
     # things that need to match
-    indices += ["nCells"]
+    if node_based:
+        indices = [q.idx for q in bases[0]["base"]]
+        indices += ["nNodes"]
+        exclude = None
+    else:
+        indices = [q.idx for q in bases[0]["base"]]
+        exclude = ["nNodes"]
+    indices += ["nCells", "Host"]
+
     df_copy = deepcopy(extra_filter(df))
-    df_copy.set_index(keys=indices, inplace=True)
-    speedup_df =  eph.helpers.compute_speedup(df_copy, bases, ignore_indices=[]).reset_index()
+    df_copy_set_idx = df_copy.set_index(keys=indices)
+    speedup_df = eph.helpers.compute_speedup(
+        df_copy_set_idx, bases, ignore_indices=[], exclude=exclude
+    ).reset_index()
+
     return speedup_df[speedup_df["executor"] != "CPU"]
 
+
+def generate_base(node_based=False):
+    """This function generates the list of base cases queries. The base case queries are used to compute the speedup.
+    If node_based is set to be true no specific number of nProcs is added to the base case query.
+
+    Returns:
+        a list case and base case queries
+    """
+
+    # TODO this needs to know nProcs beforehand
+    base_ = [
+        eph.helpers.DFQuery(idx="preconditioner", val="none"),
+        eph.helpers.DFQuery(idx="executor", val="CPU"),
+    ]
+
+    base_nla = deepcopy(base_) 
+    base_hkn = deepcopy(base_) 
+    base_smuc = deepcopy(base_) 
+
+    if not node_based:
+        base_nla.append(eph.helpers.DFQuery(idx="nProcs", val=32))
+        base_hkn.append(eph.helpers.DFQuery(idx="nProcs", val=76))
+        base_smuc.append(eph.helpers.DFQuery(idx="nProcs", val=112))
+
+    return [
+        # {
+        #     "case": [
+        #         eph.helpers.DFQuery(idx="Host", val="nla"),
+        #     ],
+        #     "base": base_nla,
+        # },
+        {
+            "case": [
+                eph.helpers.DFQuery(idx="Host", val="hkn"),
+            ],
+            "base": base_hkn,
+        },
+    ]
+        {
+            "case": [
+                eph.helpers.DFQuery(idx="Host", val="i20"),
+            ],
+            "base": base_smuc,
+        },
+
+
+def compute_fvops(df):
+    """this function computes fvops"""
+    df["fvOps"] = df["nCells"] / df["TimeStep"] * 1000.
+    return df
+
+def compute_fvops_piter(df):
+    """this function computes nCellsPerCU"""
+    df["fvOpsPIter"] = df["nCells"] / df["TimeStep"] * 1000 / df["p_NoIterations"]
+    return df
+
+def compute_nCellsPerCU(df):
+    """this function computes nCellsPerCU"""
+    df["nCellsPerRank"] = df["nCells"] / df["nProcs"]
+    return df
 
 
 def main(campaign, comparisson=None):
@@ -93,76 +194,46 @@ def main(campaign, comparisson=None):
     json_file = post_pro_dir / "results.json"
     df = pd.read_json(json_file)
 
-    bases = [
-    {
-        "case": [
-            eph.helpers.DFQuery(idx="Host", val="nla"),
-            ],
-        "base" : [
-            # TODO this needs to know nProcs beforehand
-            eph.helpers.DFQuery(idx="nProcs", val=32),
-            eph.helpers.DFQuery(idx="preconditioner", val="none"),
-            eph.helpers.DFQuery(idx="executor", val="CPU"),
-            ]
-    },
-    {
-        "case": [
-            eph.helpers.DFQuery(idx="Host", val="hkn"),
-            ],
-        "base" : [
-            # TODO this needs to know nProcs beforehand
-            eph.helpers.DFQuery(idx="nProcs", val=76),
-            eph.helpers.DFQuery(idx="preconditioner", val="none"),
-            eph.helpers.DFQuery(idx="executor", val="CPU"),
-            ]
-    },
-    {
-        "case": [
-            eph.helpers.DFQuery(idx="Host", val="i20"),
-            ],
-        "base" : [
-            # TODO this needs to know nProcs beforehand
-            eph.helpers.DFQuery(idx="nProcs", val=112),
-            eph.helpers.DFQuery(idx="preconditioner", val="none"),
-            eph.helpers.DFQuery(idx="executor", val="CPU"),
-            ]
-    },
-    ]
-
-    speedup = compute_speedup(df, bases)
-    speedup.to_json(path_or_buf=post_pro_dir/"speedup_results.json")
-
+    df = compute_fvops(df)
+    df = compute_fvops_piter(df)
+    df = compute_nCellsPerCU(df)
 
     unprecond = lambda x: x[x["preconditioner"] == "none"]
-    for x, c in [("nCells", "nProcs"), ("nProcs", "nCells")]:
-        for y in ["TimeStep", "SolveP"]:
-            plotter(
-                x=x,
-                y=y,
-                color=c,
-                style="solver_p",
-                post_pro_dir=post_pro_dir,
-                plot_type="line",
-                col="Host",
-                log=True,
-                df=df,
-                df_filter=Df_filter("unpreconditioned", unprecond),
-            )
-
-            plotter(
-                x=x,
-                y=y,
-                color=c,
-                style="solver_p",
-                post_pro_dir=post_pro_dir,
-                plot_type="line",
-                col="Host",
-                log=True,
-                df=df,
-                df_filter=Df_filter(
-                    "unprecond_speedup", lambda df: compute_speedup(df, bases, unprecond)
-                ),
-            )
+    for filt in [
+        Df_filter("unpreconditioned", unprecond),
+        Df_filter(
+            "unprecond_speedup",
+            lambda df: compute_speedup(df, generate_base(node_based=False), unprecond),
+        ),
+        Df_filter(
+            "unprecond_speedup_nNodes",
+            lambda df: compute_speedup(
+                df, generate_base(node_based=True), unprecond, node_based=True
+            ),
+        ),
+    ]:
+        for x, c in [
+            ("nCells", "nProcs"),
+            ("nProcs", "nCells"),
+            ("nNodes", "nCells"),
+            ("nCellsPerRank", "nCells"),
+        ]:
+            try:
+                for y in ["TimeStep", "SolveP", "fvOps", "fvOpsPIter"]:
+                    plotter(
+                        x=x,
+                        y=y,
+                        color=c,
+                        style="solver_p",
+                        post_pro_dir=post_pro_dir,
+                        plot_type="line",
+                        col="Host",
+                        log=True,
+                        df=df,
+                        df_filter=filt,
+                    )
+            except Exception as e:
+                print(e)
 
     # comparisson against other results
     try:
