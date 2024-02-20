deeb26f936b4af3f9b52ea9cc5114ce0910af6bc
Gregor Olenik <go@hpsim.de>
2024-02-20 18:16:13 +0100

    require same CPUGPU ratio for speedup plot

--- LidDrivenCavity3D/assets/plot.py
+++ LidDrivenCavity3D/assets/plot.py
@@ -262,7 +262,7 @@ def unprecond_rank_range(df):
     )
 
     df = df[mapping]
-    print(df.to_string())
+    print("unprecond_rank_range", df.to_string())
     return df[df["deviceRankOverSubscription"] < 10]
 
 
